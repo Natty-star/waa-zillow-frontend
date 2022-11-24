561f064ba1b32d446e82759bdcc8ab1726c4ca94
yemane <yemane.hadis@gmail.com>
2022-11-24 15:46:45 -0600

    add property list

--- src/components/properties/Properties.js
+++ src/components/properties/Properties.js
@@ -1,9 +1,133 @@
 import React from 'react'
 import './properties.css'
+import Property from './property/Property';
 import SearchInput from './search/SearchInput'
+import { useState } from 'react';
 export default function Properties() {
+
+const [properties, setProperties] = useState([
+    {
+        id: "637c75f892731430d96bac2d",
+        category: "BUY",
+        homeType: null,
+        type: "Guest House",
+        bedrooms: 2,
+        title: "first house",
+        description: "this is an amazing house",
+        price: 1000,
+        status: false,
+        ownerEmail: "merhatsidk29@gmail.com",
+        images: [
+            "https://property-managment.s3.amazonaws.com/1669101047837_png"
+        ],
+        location: [
+            45.34,
+            23.34
+        ]
+    },
+    {
+        id: "637c75f892731430d96bac2d",
+        category: "BUY",
+        homeType: null,
+        type: "Apartment",
+        bedrooms: 2,
+        title: "First house",
+        description: "this is an amazing house",
+        price: 1000,
+        status: false,
+        ownerEmail: "merhatsidk29@gmail.com",
+        images: [
+            "https://property-managment.s3.amazonaws.com/1669101047837_png"
+        ],
+        location: [
+            45.34,
+            23.34
+        ]
+    }, 
+    {
+        id: "637c75f892731430d96bac2d",
+        category: "Rent",
+        homeType: null,
+        type: "guest house",
+        bedrooms: 10,
+        title: "first house",
+        description: "this is an amazing house",
+        price: 100000,
+        status: false,
+        ownerEmail: "merhatsidk29@gmail.com",
+        images: [
+            "https://property-managment.s3.amazonaws.com/1669101047837_png"
+        ],
+        location: [
+            45.34,
+            23.34
+        ]
+    },
+    {
+        id: "637c75f892731430d96bac2d",
+        category: "Rent",
+        homeType: null,
+        type: "guest house",
+        bedrooms: 2,
+        title: "first house",
+        description: "this is an amazing house",
+        price: 1000,
+        status: false,
+        ownerEmail: "merhatsidk29@gmail.com",
+        images: [
+            "https://property-managment.s3.amazonaws.com/1669101047837_png"
+        ],
+        location: [
+            45.34,
+            23.34
+        ]
+    },
+    {
+        id: "637c75f892731430d96bac2d",
+        category: "Rent",
+        homeType: null,
+        type: "guest house",
+        bedrooms: 2,
+        title: "first house",
+        description: "this is an amazing house",
+        price: 1000,
+        status: false,
+        ownerEmail: "merhatsidk29@gmail.com",
+        images: [
+            "https://property-managment.s3.amazonaws.com/1669101047837_png"
+        ],
+        location: [
+            45.34,
+            23.34
+        ]
+    },
+    {
+        id: "637c75f892731430d96bac2d",
+        category: "Rent",
+        homeType: null,
+        type: "guest house",
+        bedrooms: 2,
+        title: "first house",
+        description: "this is an amazing house",
+        price: 1000,
+        status: false,
+        ownerEmail: "merhatsidk29@gmail.com",
+        images: [
+            "https://property-managment.s3.amazonaws.com/1669101047837_png"
+        ],
+        location: [
+            45.34,
+            23.34
+        ]
+    }
+]);
+    const propertyListHTML = properties.map(property =>{
+        return <Property property={property}/>
+    })
+
   return (
-    <div className='cover'>
+    <>
+       <div className='cover'>
         <div className='center-content center container'>
             <div >
               <form className=''>
@@ -12,5 +136,8 @@ export default function Properties() {
             </div>
         </div> 
     </div>
+    {propertyListHTML}
+    </>
+ 
   )
 }
--- src/components/properties/property/Property.js
+++ src/components/properties/property/Property.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./property.css";
-export default function Property() {
+
+export default function Property(props) {
+  const property = props.property;
   return (
     <>
       <div class="card ">
@@ -13,30 +15,33 @@ export default function Property() {
 
         <div className="card-content ">
           <div className="left">
-            <h6>$ 80,000</h6>
-            
+            <h6>$ {property.price}</h6>
           </div>
-          <div className="right">
-            <a class="waves-effect waves-light btn right">BUY</a>
-          </div>
+          {property.category == "BUY" ? (
+            <div className="right">
+              <a  class="waves-effect waves-light btn right">BUY</a>
+            </div>
+          ) : (
+            <div className="right">
+              <a class="waves-effect waves-light btn right">RENT</a>
+            </div>
+          )}
         </div>
-        <br/>
+        <br />
         <div className="divider"></div>
 
-        <div className="card-action"> 
-          <p  className="tail waves-effect waves-light">
+        <div className="card-action">
+          <p className="tail waves-effect waves-light">
             <i class="material-icons left">straighten</i> 1200 sq ft
           </p>
 
-          <p  className=" tail waves-effect waves-light">
-            <i class="material-icons left">bed</i>3
+          <p className=" tail waves-effect waves-light">
+            <i class="material-icons left">bed</i>{property.bedrooms}
           </p>
           <p className="tail waves-effect waves-light">
             <i className="material-icons left">shower</i>12
-        </p>
+          </p>
         </div>
-
-       
       </div>
     </>
   );
