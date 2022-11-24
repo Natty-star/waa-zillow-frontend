d7ad421a36501ad454b3e3f0e09f2917b51f97f4
yemane <yemane.hadis@gmail.com>
2022-11-24 14:23:30 -0600

    Search

--- src/components/properties/Properties.js
+++ src/components/properties/Properties.js
@@ -1,7 +1,16 @@
 import React from 'react'
-
+import './properties.css'
+import SearchInput from './search/SearchInput'
 export default function Properties() {
   return (
-    <div>Properties</div>
+    <div className='cover'>
+        <div className='center-content center container'>
+            <div >
+              <form className=''>
+                   <SearchInput/>
+              </form>
+            </div>
+        </div> 
+    </div>
   )
 }
